e9463108fff2bb7b0bc8ac020a23dad46606e04e
https123456789 <famlilylandon01@outlook.com>
2023-03-06 19:19:03 +0000

    feat: add types to components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,16 @@
-export default function RootLayout({ children }) {
-  return (
-    <html lang="en">
-      <head>
-        <title>AlumLinC</title>
-      </head>
-      <body>{children}</body>
-    </html>
-  );
+import { ReactNode } from "react";
+
+export type RootLayoutProps = {
+    children: ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+    return (
+        <html lang="en">
+            <head>
+                <title>AlumLinC</title>
+            </head>
+            <body>{children}</body>
+        </html>
+    );
 }
