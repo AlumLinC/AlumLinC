55c20e7d7ba5b8ca0e4142309fac46d86efc6554
Ben Landon <81256789+https123456789@users.noreply.github.com>
2023-03-09 15:57:46 -0800

    Revert "Deploy basic site to test hosting (#1)"
    This reverts commit f8385d9e9490d350c1280791997e98835571aaae.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,40 +1,10 @@
-import type { ReactNode } from "react";
-import NavBar from "components/navbar";
-import { ThemeProvider } from "contexts/theme";
-
-import "styles/reset.css";
-import "styles/global.css";
-
-export type RootLayoutProps = {
-    children: ReactNode;
-};
-
-/**
- * Root of the page
- */
-export default function RootLayout({ children }: RootLayoutProps) {
-    return (
-        <html lang="en">
-            <head>
-                <title>AlumLinC</title>
-                {/* Google Fonts */}
-                <link rel="preconnect" href="https://fonts.googleapis.com" />
-                <link
-                    rel="preconnect"
-                    href="https://fonts.gstatic.com"
-                    crossOrigin="true"
-                />
-                <link
-                    href="https://fonts.googleapis.com/css2?family=Bubblegum+Sans&family=Nova+Round&family=Roboto:wght@100;300;400;500;700;900&display=swap"
-                    rel="stylesheet"
-                />
-            </head>
-            <body>
-                <ThemeProvider>
-                    <NavBar />
-                    {children}
-                </ThemeProvider>
-            </body>
-        </html>
-    );
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <head>
+        <title>AlumLinC</title>
+      </head>
+      <body>{children}</body>
+    </html>
+  );
 }
--- app/schools/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function SchoolsPage() {
-    return <h1>Schools</h1>;
-}
